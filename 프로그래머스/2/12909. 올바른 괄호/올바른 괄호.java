6ce14b746586f47a9fb347841cc243b0b1a66adf
lsh <126748666+lsh270609@users.noreply.github.com>
2024-07-08 22:13:09 +0900

    Update 올바른 괄호.java

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,28 +1,28 @@
 import java.util.*;
 
 class Solution {
-    boolean solution(String s) {
-        boolean answer = true;
-        
-        Stack<Integer> stack = new Stack<Integer>();
-        for (int i = 0; i < s.length(); i++) {
-        	
-        	if(s.charAt(i) == '(') {
-        		stack.push(0);
-        	}
-        	if(s.charAt(i) == ')') {
-        		if(stack.isEmpty()) {
-        			answer = false;
-        			break;
-        		}
-        		stack.pop();
-        	}
-        }
-        
-        if(!stack.isEmpty()) {
+	boolean solution(String s) {
+		boolean answer = true;
+		
+		Stack<Integer> stack = new Stack<Integer>();
+		for (int i = 0; i < s.length(); i++) {
+			
+			if(s.charAt(i) == '(') {
+				stack.push(0);
+			}
+			if(s.charAt(i) == ')') {
+				if(stack.isEmpty()) {
+					answer = false;
+					break;
+				}
+				stack.pop();
+			}
+		}
+		
+		if(!stack.isEmpty()) {
 			answer = false;
 		}
-        
-        return answer;
-    }
+		
+		return answer;
+	}
 }
