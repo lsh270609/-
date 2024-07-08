b88db3552e0be4128226f089d3a1c400116d83e6
lsh <126748666+lsh270609@users.noreply.github.com>
2024-07-08 22:01:25 +0900

    Update 최댓값과 최솟값.java

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -1,14 +1,14 @@
 class Solution {
     public String solution(String s) {
-		String tmpArr[] = s.split(" ");
-		int tmp0 = Integer.parseInt(tmpArr[0]);
-		int min = tmp0, max = tmp0, tmp1;
-		for(String num : tmpArr) {
-			tmp1 = Integer.parseInt(num);
-			if(min > tmp1) min = tmp1;
-			else if(max < tmp1) max = tmp1;
-		}
-		String answer = min + " " + max;
-		return answer;	
+	String tmpArr[] = s.split(" ");
+	int tmp0 = Integer.parseInt(tmpArr[0]);
+	int min = tmp0, max = tmp0, tmp1;
+	for(String num : tmpArr) {
+		tmp1 = Integer.parseInt(num);
+		if(min > tmp1) min = tmp1;
+		else if(max < tmp1) max = tmp1;
+	}
+	String answer = min + " " + max;
+	return answer;	
     }
 }
