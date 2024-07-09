421801b5cc92e39e4ea74ec143c92e7e525cbf01
lsh <126748666+lsh270609@users.noreply.github.com>
2024-07-09 18:54:00 +0900

    Update 숫자의 표현.java

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
@@ -1,22 +1,22 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
-		int nh = (n != 2) ? (int) Math.round(n / 2.0) : 2;
-		int sum = 0;
+	int nh = (n != 2) ? (int) Math.round(n / 2.0) : 2;
+	int sum = 0;
 
-		for (int i = 1; i <= nh; i++) {
-			sum = 0;
-			for (int j = i; j <= nh; j++) {
-				sum += j;
-				if (sum == n) {
-					answer++;
-					break;
-				} else if (sum > n) {
-					break;
-				}
+	for (int i = 1; i <= nh; i++) {
+		sum = 0;
+		for (int j = i; j <= nh; j++) {
+			sum += j;
+			if (sum == n) {
+				answer++;
+				break;
+			} else if (sum > n) {
+				break;
 			}
 		}
-		if(n == 1 || n == 2) return answer;
-		else return answer+1;
+	}
+	if(n == 1 || n == 2) return answer;
+	else return answer+1;
     }
 }
