6ad3d609473491d26083a018348e7db9126eddc5
lsh <126748666+lsh270609@users.noreply.github.com>
2024-07-10 02:05:10 +0900

    [level 2] Title: 더 맵게, Time: 1327.19 ms, Memory: 122 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] scoville, int K) {
+        int answer = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int i : scoville) {
+            pq.offer(i);
+        }
+        while(pq.size() > 1 && pq.peek() < K) {
+            pq.offer(pq.poll() + pq.poll() * 2);
+            answer++;
+        }
+        return pq.peek() < K ? -1 : answer;
+    }
+}
