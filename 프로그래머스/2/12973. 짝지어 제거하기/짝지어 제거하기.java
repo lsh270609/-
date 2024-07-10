0c1e0f0cfd330bdd344f58873dcef5f56f7bf841
lsh <126748666+lsh270609@users.noreply.github.com>
2024-07-10 14:52:48 +0900

    [level 2] Title: 짝지어 제거하기, Time: 258.18 ms, Memory: 128 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -5,7 +5,7 @@ class Solution {
         if (s.length() % 2 == 1) return 0;
         Stack<String> stack = new Stack<>();
         for (String str : s.split("")) {
-            if (stack.size() != 0 && stack.lastElement().equals(str)) {
+            if (stack.size() != 0 && stack.peek().equals(str)) {
                 stack.pop();
             } else {
                 stack.add(str);
