586259bd7393b7a1fbd858693cdd7c8f55d47a11
lsh <126748666+lsh270609@users.noreply.github.com>
2024-07-11 14:22:12 +0900

    [level 2] Title: 스킬트리, Time: 0.74 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.java
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.java
@@ -1,16 +1,16 @@
 import java.util.*;
 
 class Solution {
-    public int solution(String skill, String[] skill_trees) {
+	public int solution(String skill, String[] skill_trees) {
 		int answer = 0;
-
+		
 		for (String curSkill : skill_trees) {
 			answer += skillTree(skill, curSkill);
 		}
-
+		
 		return answer;
 	}
-
+	
 	public int skillTree(String skill, String curSkill) {
 		Queue<String> queue1 = new LinkedList<>();
 		for (String s : skill.split("")) {
@@ -24,8 +24,8 @@ class Solution {
 			if (!skill.contains(queue2.peek())) {
 				queue2.poll();
 			} else if (queue1.peek().equals(queue2.peek())) {
-				queue1.poll();
-				queue2.poll();
+			queue1.poll();
+			queue2.poll();
 			} else return 0;
 		}
 		return 1;
