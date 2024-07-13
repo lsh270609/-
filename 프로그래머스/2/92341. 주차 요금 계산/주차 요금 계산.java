6af0bfe40e520378f640ee70384b6874317f9592
lsh <126748666+lsh270609@users.noreply.github.com>
2024-07-13 17:24:03 +0900

    [level 2] Title: 주차 요금 계산, Time: 5.40 ms, Memory: 79.4 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
@@ -2,15 +2,7 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] fees, String[] records) {
-        ArrayList<String> recordList = new ArrayList<>(Arrays.asList(records));
-        HashMap<String, Integer> count = new HashMap<>();
-        for(String record : records) {
-            count.put(record.substring(6, 10), count.getOrDefault(record, 0) + 1);
-        }
-        for(String str : count.keySet()) {
-            if(count.get(str) % 2 == 1)  recordList.add("23:59 "+str+" OUT");
-        }
-        System.out.println(recordList.toString());
+        ArrayList<String> recordList = new ArrayList<>(Arrays.asList(records));        
         HashMap<String, ArrayList<Integer>> map = new HashMap<>(); 
         ArrayList<Integer> tmp = new ArrayList<>();
         for(int i = 0; i < 3; i++){
@@ -20,7 +12,7 @@ class Solution {
             String str = record.substring(6, 10);
             int time = Integer.parseInt(record.substring(0, 2)) * 60 + Integer.parseInt(record.substring(3, 5));
             ArrayList<Integer> list = map.getOrDefault(str, new ArrayList<Integer>(tmp));
-            list.set(0, list.get(0) + 1);
+            list.set(0, record.substring(11, 13).equals("IN") ? 1 : 0);
             if(list.get(0) % 2 == 0) {
                 list.set(1, list.get(1) + time - list.get(2));
             } else {
@@ -28,7 +20,15 @@ class Solution {
             }
             map.put(str, list);
         }
-
+        
+        for(String num : map.keySet()) {
+            if(map.get(num).get(0) == 1) {
+                ArrayList<Integer> list = map.get(num);
+                list.set(1, list.get(1) + 1439 - list.get(2));
+                map.put(num, list);
+            }
+        }
+        
         ArrayList<String> keySet = new ArrayList<>(map.keySet());
         Collections.sort(keySet);
         int[] answer = new int[keySet.size()];
