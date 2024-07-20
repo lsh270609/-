8303c74b94c8e53c678a19526f8cfa413764154d
lsh <126748666+lsh270609@users.noreply.github.com>
2024-07-20 13:15:54 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 48.37 ms, Memory: 117 MB -BaekjoonHub

--- 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.java
+++ 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.java
@@ -2,52 +2,52 @@ import java.util.*;
 
 class Solution {
     public int solution(int m, int n, String[] board) {
-		int answer = 0;
-		HashSet<ArrayList<Integer>> set = new HashSet<>();
-		String[][] arr1 = new String[m][n];
-		for (int i = 0; i < m; i++) {
-			String[] arr2 = board[i].split("");
-			for (int j = 0; j < n; j++) {
-				arr1[i][j] = arr2[j];
-			}
-		}
+        int answer = 0;
+        HashSet<ArrayList<Integer>> set = new HashSet<>();
+        String[][] arr1 = new String[m][n];
+        for (int i = 0; i < m; i++) {
+            String[] arr2 = board[i].split("");
+            for (int j = 0; j < n; j++) {
+                arr1[i][j] = arr2[j];
+            }
+        }
 
-		while (true) {
-			set.clear();
-			for (int i = 0; i < m - 1; i++) {
-				for (int j = 0; j < n - 1; j++) {
-					String x1 = arr1[i][j], y1 = arr1[i][j + 1];
-					String x2 = arr1[i + 1][j], y2 = arr1[i + 1][j + 1];
-					if (x1.equals("0")) continue;
-					else if (x1.equals(y1) && x1.equals(x2) && y1.equals(y2)) {
-						set.add(new ArrayList<Integer>(List.of(i, j)));
-						set.add(new ArrayList<Integer>(List.of(i, j + 1)));
-						set.add(new ArrayList<Integer>(List.of(i + 1, j)));
-						set.add(new ArrayList<Integer>(List.of(i + 1, j + 1)));
-					}
-				}
-			}
-			if (set.isEmpty()) break;
-			else answer += set.size();
-            
-			for (int i = 0; i < n; i++) {
-				ArrayList<String> list = new ArrayList<>();
-				for (int j = m - 1; j >= 0; j--) {
-					if (!set.contains(new ArrayList<Integer>(List.of(j, i)))) {
-						list.add(arr1[j][i]);
-					}
-				}
-				Iterator<String> it = list.iterator();
-				int k = 0;
-				while (it.hasNext()) {
-					arr1[m - 1 - k++][i] = it.next();
-				}
-				for (int j = m - k - 1; j >= 0; j--) {
-					arr1[j][i] = "0";
-				}
-			}
-		}
-        
-		return answer;
-	}
+        while (true) {
+            set.clear();
+            for (int i = 0; i < m - 1; i++) {
+                for (int j = 0; j < n - 1; j++) {
+                    String x1 = arr1[i][j], y1 = arr1[i][j + 1];
+                    String x2 = arr1[i + 1][j], y2 = arr1[i + 1][j + 1];
+                    if (x1.equals("0")) continue;
+                    else if (x1.equals(y1) && x1.equals(x2) && y1.equals(y2)) {
+                        set.add(new ArrayList<Integer>(List.of(i, j)));
+                        set.add(new ArrayList<Integer>(List.of(i, j + 1)));
+                        set.add(new ArrayList<Integer>(List.of(i + 1, j)));
+                        set.add(new ArrayList<Integer>(List.of(i + 1, j + 1)));
+                    }
+                }
+            }
+            if (set.isEmpty()) break;
+            else answer += set.size();
+
+            for (int i = 0; i < n; i++) {
+                ArrayList<String> list = new ArrayList<>();
+                for (int j = m - 1; j >= 0; j--) {
+                    if (!set.contains(new ArrayList<Integer>(List.of(j, i)))) {
+                        list.add(arr1[j][i]);
+                    }
+                }
+                Iterator<String> it = list.iterator();
+                int k = 0;
+                while (it.hasNext()) {
+                    arr1[m - 1 - k++][i] = it.next();
+                }
+                for (int j = m - k - 1; j >= 0; j--) {
+                    arr1[j][i] = "0";
+                }
+            }
+        }
+
+        return answer;
+    }
 }
