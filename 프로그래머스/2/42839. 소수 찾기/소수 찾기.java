ed77f132d4eb11d2ca4b0db9f1c04aa0e98762b1
lsh <126748666+lsh270609@users.noreply.github.com>
2024-07-27 17:01:01 +0900

    [level 2] Title: 소수 찾기, Time: 10.58 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 class Solution {
     public HashSet<Integer> set;
-    
+
     public int solution(String numbers) {
         ArrayList<Integer> list = new ArrayList<>();
         for (String str : numbers.split("")) {
@@ -10,32 +10,30 @@ class Solution {
         }
         set = new HashSet<>();
         HashSet<Integer> numberSet = new HashSet<>();
-        
-        for(int i = 0; i < list.size(); i++) {
+
+        for (int i = 0; i < list.size(); i++) {
             numberSet.add(i);
             bfs(list.get(i), list, numberSet);
             numberSet.remove(i);
         }
-        
+
         return set.size();
     }
-    
+
     public void bfs(int num, ArrayList<Integer> list, HashSet<Integer> numberSet) {
-        if(primeCheck(num)) set.add(num);
-        for(int j = 0; j < list.size(); j++) {
-            if(numberSet.contains(j)) continue;
+        if (primeCheck(num)) set.add(num);
+        for (int j = 0; j < list.size(); j++) {
+            if (numberSet.contains(j)) continue;
             numberSet.add(j);
             bfs(num * 10 + list.get(j), list, numberSet);
             numberSet.remove(j);
         }
     }
-    
+
     public boolean primeCheck(int i) {
         if (i < 2) return false;
-        else {
-            for(int j = 2; j <= (int) Math.sqrt(i); j++) {
-                if (i % j == 0) return false;
-            }
+        for(int j = 2; j <= (int) Math.sqrt(i); j++) {
+            if (i % j == 0) return false;
         }
         return true;
     }
