c2a139949cfd3b0ff980f8da0824bad9243954ed
lsh <126748666+lsh270609@users.noreply.github.com>
2024-07-30 16:41:41 +0900

    [level 2] Title: 삼각 달팽이, Time: 20.13 ms, Memory: 124 MB -BaekjoonHub

---
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int[] solution(int n) {
+        int nf = 0;
+        for (int i = 1; i <= n; i++) {
+            nf += i;
+        }
+        int[] answer = new int[nf];
+        int[][] arr = new int[n + 1][n];
+        int row = 0, col = 0, timing = 0, number = 0;
+        int tmpN = n;
+        
+        while (true) {
+            for (int i = 1; i <= tmpN; i++) {
+                if (timing % 3 == 0) {
+                    arr[++row][col] = number + i;
+                } else if ((timing - 1) % 3 == 0) {
+                    arr[row][++col] = number + i;
+                } else {
+                    arr[--row][--col] = number + i;
+                }
+            }
+            number += tmpN;
+            timing++;
+            if (--tmpN == 0) break;
+        }
+
+        int cnt = 0, idx = 0;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j <= cnt; j++) {
+                answer[idx++] = arr[i][j];
+            }
+            cnt++;
+        }
+
+        return answer;
+    }
+}
