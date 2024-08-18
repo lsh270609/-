a48fa2aedf3f6413d28d0d620953d9f4dd5816c0
lsh <126748666+lsh270609@users.noreply.github.com>
2024-08-18 23:23:50 +0900

    [level 2] Title: 테이블 해시 함수, Time: 16.29 ms, Memory: 140 MB -BaekjoonHub

--- 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.java
+++ 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.java
@@ -1,53 +1,28 @@
 import java.util.*;
 
 class Solution {
+    public int cpCol;
+    
     public int solution(int[][] data, int col, int row_begin, int row_end) {
+        cpCol = --col;
         int row = data.length, column = data[0].length;
-        col--;
-        for (int i = 0; i < row; i++) {
-            int[] t1 = data[i];
-            for (int j = i + 1; j < row; j++) {
-                int[] t2 = data[j];
-                if (t1[col] > t2[col]) {
-                    swap(t1, t2);
-                } else if (t1[col] == t2[col]) {
-                    if (t1[0] < t2[0]) {
-                        swap(t1, t2);
-                    }
-                }
+        Arrays.sort(data, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[cpCol] == o2[cpCol] ? o2[0] - o1[0] : o1[cpCol] - o2[cpCol];
             }
-        }
+        });
         
         int idx = 0;
+        int answer = 0;
         int[] hashArr = new int[row_end - row_begin + 1];
         for (int i = row_begin - 1; i <= row_end - 1; i++) {
             for (int j = 0; j < column; j++) {
                 hashArr[idx] += data[i][j] % (i + 1);
             }
-            idx++;
-        }
-        
-        int answer = hashArr[0];
-        for (int i = 1; i < hashArr.length; i++) {
-            answer = answer ^ hashArr[i];
+            answer = answer ^ hashArr[idx++];
         }
         
         return answer;
-    }
-    
-    public void swap(int[] arr1, int[] arr2) {
-        int[] cpArr = copy(arr2);
-        for (int k = 0; k < arr1.length; k++) {
-            arr2[k] = arr1[k];
-            arr1[k] = cpArr[k];
-        }
-    }
-    
-    public int[] copy(int[] arr) {
-        int[] cpArr = new int[arr.length];
-        for (int i = 0; i < arr.length; i++) {
-            cpArr[i] = arr[i];
-        }
-        return cpArr;
     }
 }
